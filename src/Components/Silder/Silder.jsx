88e2ac106e7889495fc56beeda7f356ee8812470
Sharif101 <sharif009009@gmail.com>
2024-05-12 16:04:56 +0600

    first commit

---
+++ src/Components/Silder/Silder.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import classes from "./Silder.module.css";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import img1 from "../../images/1.jpg";
+import img2 from "../../images/2.jpg";
+import img3 from "../../images/3.jpg";
+import img4 from "../../images/4.jpg";
+import img5 from "../../images/5.jpg";
+import img6 from "../../images/6.jpg";
+
+export default function Silder() {
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  return (
+    <div className={classes.container}>
+      <Slider {...settings}>
+        <div className={classes.div}>
+          <div className={classes.img}>
+            <img src={img1} alt="" />
+          </div>
+          <h3>1</h3>
+        </div>
+        <div className={classes.div}>
+          <div className={classes.img}>
+            <img src={img2} alt="" />
+          </div>
+          <h3>2</h3>
+        </div>
+        <div className={classes.div}>
+          <div className={classes.img}>
+            <img src={img3} alt="" />
+          </div>
+          <h3>3</h3>
+        </div>
+        <div className={classes.div}>
+          <div className={classes.img}>
+            <img src={img4} alt="" />
+          </div>
+          <h3>4</h3>
+        </div>
+        <div className={classes.div}>
+          <div className={classes.img}>
+            <img src={img5} alt="" />
+          </div>
+          <h3>5</h3>
+        </div>
+        <div className={classes.div}>
+          <div className={classes.img}>
+            <img src={img6} alt="" />
+          </div>
+          <h3>6</h3>
+        </div>
+      </Slider>
+    </div>
+  );
+}
